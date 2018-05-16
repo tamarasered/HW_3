ebcaffdff6a37ece5be75c53b85fd6aee241fe1f
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-16 11:04:25 -0400

    saving barchart.R

--- barchart.R
+++ barchart.R
@@ -14,8 +14,9 @@ df <- read.csv('clean_data.csv')
 
 ####################################
 ##### write output file ############
-# add your R code to write bar_NPS_Det.png
+# add your R code to write bar_NPS_Pro.png
 ####################################
+
 
 
 
