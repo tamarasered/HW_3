76b51b7518b2a65aaa9fb806e3289f3c3273b0b8
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-16 15:01:30 -0400

    saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -25,40 +25,18 @@ myPlot
 dev.off()
 
 # PART A.2: Generate a boxplot for each NPS Type. 
-
-#Creating data frames for types of NPS
-promoters <- clean_data[clean_data$Likelihood_Recommend_H > 8,]
-passives <- clean_data[clean_data$Likelihood_Recommend_H > 6 & clean_data$Likelihood_Recommend_H<9,]
-detractors <- clean_data[clean_data$Likelihood_Recommend_H < 7,]
-
-#Creating corresponding boxplots. 
-# The data for promoters.
-myPlotPromoters <- ggplot(promoters)
-# The aesthetic for promoters.
-myPlotPromoters <- myPlotPromoters + aes(x=promoters$Likelihood_Recommend_H, y=promoters$Likelihood_Recommend_H)
-# The geometry for promoters.
-myPlotPromoters <- myPlotPromoters + geom_boxplot(fill="yellow", col="black")
+# The data.
+myPlot_NPS <- ggplot(clean_data)
+# The aesthetic.
+myPlot_NPS <- myPlot_NPS + aes(x=clean_data$NPS_Type, y=clean_data$Likelihood_Recommend_H)
+# The geometry.
+myPlot_NPS <- myPlot_NPS + geom_boxplot(fill="yellow", col="black")
 # Invoke the plot to draw it. 
-myPlotPromoters
-#Creating the png file of the boxplot for promoters.
-png(filename="box_NPS_promoters.png")
-myPlotPromoters
+myPlot_NPS
+#Creating the png file of the boxplot.
+png(filename="box_NPS.png")
+myPlot_NPS
 dev.off()
-
-# The data for passives
-myPlotPassives <- ggplot(passives)
-# The aesthetic for passives
-myPlotPassives <- myPlotPassives + aes(x=passives$Likelihood_Recommend_H, y=passives$Likelihood_Recommend_H)
-# The geometry for passives
-myPlotPassives <- myPlotPassives + geom_boxplot(fill="yellow", col="black")
-# Invoke the plot to draw it. 
-myPlotPassives
-#Creating the png file of the boxplot for passives
-png(filename="box_NPS_passives.png")
-myPlotPassives
-dev.off()
-
-
 # Create a new data frame with 
 ## end your R code and logic 
 
