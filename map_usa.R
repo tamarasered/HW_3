71d5defe2600d3b647f269b42e47a188e5e28729
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-16 11:12:16 -0400

    creating map_usa.R

---
+++ map_usa.R
@@ -0,0 +1,19 @@
+#####################################
+##### Do NOT delete this block #####
+setwd(Sys.getenv('PROJECT_HOME'))
+####################################
+
+####################################
+## write code to read input csv into data frame
+df <- read.csv()
+####################################
+
+## start writing your R code from here
+
+## end your R code and logic 
+
+####################################
+##### write output file ############
+# add your R code to write output file
+####################################
+
