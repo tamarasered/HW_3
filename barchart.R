e458e066d9e4ba308f7057506bbf25335246f7d3
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-16 12:21:41 -0400

    saving barchart.R

--- barchart.R
+++ barchart.R
@@ -9,20 +9,26 @@ df <- read.csv('clean_data.csv')
 ####################################
 
 ## start writing your R code from here
-
+library(ggplot2)
 #Part B: 1
 # Creating a vector LTR
 LTR <- df$Likelihood_Recommend_H
 
 #Creating types of NPS
-promoters <- (sum(LTR > 8))/length(LTR)
-passives <- (sum(LTR > 6 & LTR<9))/length(LTR)
-detractors <- (sum(LTR < 7))/length(LTR)
-LTR
-Type<- 
+promoters <- (sum(LTR > 8))/length(LTR)*100
+passives <- (sum(LTR > 6 & LTR<9))/length(LTR)*100
+detractors <- (sum(LTR < 7))/length(LTR)*100
 
-png(filename="hist_NPS10.png")
-hist(AVG)
+#Creating vectors for the NPS types and values  
+Type<- c(promoters, passives, detractors)
+Names <- c("promoters", "passives", "detractors")
+
+#Generating a bar chart
+MyPlot <- ggplot() + geom_bar(aes(x=Names,y=Type), stat="identity") + ggtitle(paste("Overall NPS value:", round(mean(Type)))) + xlab("NPS types") + ylab("NPS type values")
+
+#Generating PNG file
+png(filename="bar_NPS.png")
+MyPlot
 dev.off()
 
 ## end your R code and logic 
