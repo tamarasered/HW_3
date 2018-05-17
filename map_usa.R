d4b09f5060cf5329d1a983b838269217dca89373
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-16 23:25:09 -0400

    saving map_usa.R

--- map_usa.R
+++ map_usa.R
@@ -18,29 +18,28 @@ clean_data <- read.csv('clean_data.csv')
 
 # First we need to have data with only USA information. For that we need to 
 # keep only those rows where GUEST_COUNTRY_R is UNITED STATES.
-usa_clean_data <- clean_data [clean_data$GUEST_COUNTRY_R == "UNITED STATES",] 
-# To make sure there are no blanks in states column for each UNITED STATES row
+usa_clean_data <- clean_data[clean_data$GUEST_COUNTRY_R == "UNITED STATES",] 
+# To make sure there are no blanks/nas in states column for each UNITED STATES row
 # we need to clean the data for states as well as follows.
 states_clean_data <- usa_clean_data[!is.na(usa_clean_data$STATE_R),]
+states_clean_data <- states_clean_data[!(states_clean_data$STATE_R == ""),]
 
 # We can now proceed with creating the US map. 
 library(ggplot2)
 library(ggmap)
 
-#Crearing ne data frame 
+#Crearing new data frame 
 
-new_df <- data.frame("state" = states_clean_data$STATE_R, "ltr" = states_clean_data$Likelihood_Recommend_H)
-
-aggregateState <- setNames(aggregate(new_df$ltr, by=list(new_df$state), FUN=mean), c('state', 'meanLTR'))
-aggregateState$state <- state.name[match(as.character(aggregateState$state), state.abb)]
-aggregateState$state <- tolower(aggregateState$state)
+new_df <- setNames(aggregate(states_clean_data$Likelihood_Recommend_H, by=list(states_clean_data$STATE_R), FUN=mean), c('STATE_R', 'MEAN_LTR'))
+new_df$STATE_R <- state.name[match(as.character(new_df$STATE_R), state.abb)]
+new_df$STATE_R <- tolower(new_df$STATE_R)
 
 us <- map_data("state")
-
-us$meanLTR <- aggregateState$meanLTR[match(us$region,aggregateState$state)]
-
-map_usa_LTR <- ggplot(data = us) + 
-  geom_polygon(aes(x = long, y = lat, fill=meanLTR, group = group), col = "black") +
+# Create a new column in us data frame with corresponding matching LTR.
+us$MEAN_LTR <- new_df$MEAN_LTR[match(us$region,new_df$STATE_R)]
+# Creating the map.
+map_usa_LTR <- ggplot(us) + 
+  geom_polygon(aes(x = long, y = lat, fill=MEAN_LTR, group = group), color = "black") +
   scale_fill_gradient(low="white", high="green")
 
 map_usa_LTR
@@ -55,28 +54,22 @@ dev.off()
 # size based on the average LTR for the hotels in that city.Have the color of 
 # the points range from black to red (using ‘scale_colour_gradient’).
 
-lonlat  <- geocode(c('burlington vermont', 'orlando florida', 'nashville tennessee'))
-lonlat1 <- geocode(c('burlington vermont'))
-lonlat2 <- geocode(c('orlando florida'))
-lonlat3 <- geocode(c('nashville tennessee'))
-lonlat[1,] <- lonlat1
-lonlat[2,] <- lonlat2
-lonlat[3,] <- lonlat3
+Locations  <- geocode(c('Orlando FL',  'Nashville TN', 'Burlington VT'))
 
-cityData <- setNames(aggregate(c(states_clean_data$Likelihood_Recommend_H), by=list(states_clean_data$City_PL), FUN=mean), 
-                     c('city', 'LTR'))
-lonlat$meanLTR <- c(cityData$LTR[as.character(cityData$city) == 'Burlington'], 
-                    cityData$LTR[as.character(cityData$city) == 'Orlando'],
-                    cityData$LTR[as.character(cityData$city) == 'Nashville'])
+new_df2 <- setNames(aggregate(states_clean_data$Likelihood_Recommend_H, by=list(states_clean_data$City_PL), FUN=mean), 
+                     c('City_PL', 'MEAN_LTR'))
+Locations$MEAN_LTR <- c(new_df2$MEAN_LTR[as.character(new_df2$City_PL) == 'Orlando'], 
+                        new_df2$MEAN_LTR[as.character(new_df2$City_PL) == 'Nashville'],
+                        new_df2$MEAN_LTR[as.character(new_df2$City_PL) == 'Burlington'])
 
-map_usa_LTR <- map_usa_LTR +
-  geom_point(data=lonlat, aes(x=lon, y=lat, color=meanLTR, size=meanLTR)) +
+map_usa_LTR_FL_TN_VT <- map_usa_LTR +
+  geom_point(data=Locations, aes(x=lon, y=lat, color=MEAN_LTR, size=MEAN_LTR)) +
   scale_colour_gradient(low = 'black', high='red')
-map_usa_LTR
+map_usa_LTR_FL_TN_VT
 
 # Creating png for the map.
 png(filename="map_usa_LTR_FL_TN_VT.png")
-map_usa_LTR
+map_usa_LTR_FL_TN_VT
 dev.off()
 
 ## end your R code and logic 
