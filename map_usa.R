effec3822f63b8647206334d3d959bba3e19defc
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-16 22:34:59 -0400

    saving map_usa.R

--- map_usa.R
+++ map_usa.R
@@ -5,79 +5,81 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-clean_data <- read.csv('clean_data.csv')
+df <- read.csv('clean_data.csv')
 ####################################
 
 ## start writing your R code from here
-
-# Part E:Generate a Map of the USA, where the color of the state represents 
-# the average LTR for people from that state. Use tapply to generate 
-# the average ltr for each state. Hint: Create a subset dataframe, with only 
-# surveys of US hotels. Hint 2: it’s not where the hotel is located, it’s 
-# where the person is who made the reservation
+# Part E:Generate a Map of the USA, where the color of the state represents the average LTR for people from that state.
 
 # First we need to have data with only USA information. For that we need to 
 # keep only those rows where GUEST_COUNTRY_R is UNITED STATES.
-usa_clean_data <- clean_data [clean_data$GUEST_COUNTRY_R == "UNITED STATES",] 
-# To make sure there are no blanks in states column for each UNITED STATES row
-# we need to clean the data for states as well as follows.
-states_clean_data <- usa_clean_data[!is.na(usa_clean_data$STATE_R),]
 
-# We can now proceed with creating the US map. 
+#Calling the libraries
+library(dplyr)
 library(ggplot2)
 library(ggmap)
 
-#Crearing ne data frame 
+#Filtering the dataframe by "United States"
+StateDF<- df %>% filter(GUEST_COUNTRY_R=="UNITED STATES") %>% select(STATE_R, Likelihood_Recommend_H)
 
-new_df <- data.frame("state" = states_clean_data$STATE_R, "ltr" = states_clean_data$Likelihood_Recommend_H)
+#Calculating mean LTR for each state
+LTRmean <- tapply(StateDF$Likelihood_Recommend_H, StateDF$STATE_R, mean)
 
-aggregateState <- setNames(aggregate(new_df$ltr, by=list(new_df$state), FUN=mean), c('state', 'meanLTR'))
-aggregateState$state <- state.name[match(as.character(aggregateState$state), state.abb)]
-aggregateState$state <- tolower(aggregateState$state)
+#Filtering the vector by 50 states
+LTRmean <- NPSmean[names(NPSmean) %in% state.abb]
 
-us <- map_data("state")
+#Creating a dataframe of 50 states and mean NPS
+LTRmeans <- data.frame(state=(names(LTRmean)), LTR=LTRmean)
 
-us$meanLTR <- aggregateState$meanLTR[match(us$region,aggregateState$state)]
+#Adding 'state name' in lowercase format to the dataframe
+LTRmeans <- LTRmeans %>% mutate(statename=tolower(state.name[match(LTRmeans$state,state.abb)]))
 
-map_usa_LTR <- ggplot(data = us) + 
-  geom_polygon(aes(x = long, y = lat, fill=meanLTR, group = group), col = "black") +
-  scale_fill_gradient(low="white", high="green")
+# We can now proceed with creating the US map.
+#Getting the US map
+US <- map_data("state") 
 
-map_usa_LTR
+USmap <- ggplot(LTRmeans, aes(map_id = statename)) + geom_map(map = US, aes(fill=LTR), color="black") 
+USmap <- USmap + expand_limits(x = US$long, y = US$lat)
+USmap <- USmap + coord_map() + ggtitle("Map of USA filled by mean LTR")
+USmap<- USmap + scale_fill_gradient(low = "lightgreen",high = "darkgreen", breaks=c(8.1,8.5,9,9.5,10))
+
 
 # Creating png for the map.
 png(filename="map_usa_LTR.png")
-map_usa_LTR
+USmap
 dev.off()
 
-# Part F:	Put three points on the colored map of the US – one for Orlando FL, 
+##################################################
+
+# Part F:	Put three points on the colored map of the US one for Orlando FL, 
 # one for Nashville, TN and one for Burlington VT. Have the points color and
 # size based on the average LTR for the hotels in that city.Have the color of 
-# the points range from black to red (using ‘scale_colour_gradient’).
+# the points range from black to red (using scale_colour_gradient).
 
-lonlat  <- geocode(c('burlington vermont', 'orlando florida', 'nashville tennessee'))
-lonlat1 <- geocode(c('burlington vermont'))
-lonlat2 <- geocode(c('orlando florida'))
-lonlat3 <- geocode(c('nashville tennessee'))
-lonlat[1,] <- lonlat1
-lonlat[2,] <- lonlat2
-lonlat[3,] <- lonlat3
+#Getting coordinates for three cities and creating a new dataframe
+point  <- geocode(c('burlington vermont', 'orlando florida', 'nashville tennessee'))
 
-cityData <- setNames(aggregate(c(states_clean_data$Likelihood_Recommend_H), by=list(states_clean_data$City_PL), FUN=mean), 
-                     c('city', 'LTR'))
-lonlat$meanLTR <- c(cityData$LTR[as.character(cityData$city) == 'Burlington'], 
-                    cityData$LTR[as.character(cityData$city) == 'Orlando'],
-                    cityData$LTR[as.character(cityData$city) == 'Nashville'])
+#Creating a vector for three cities
+threestates <- c("VT", "FL", "TN") 
 
-map_usa_LTR <- map_usa_LTR +
-  geom_point(data=lonlat, aes(x=lon, y=lat, color=meanLTR, size=meanLTR)) +
-  scale_colour_gradient(low = 'black', high='red')
-map_usa_LTR
+#adding mean LTR to the dataframe point
+point$LTR <- c(LTRmeans$LTR[match(threestates,LTRmeans$state)])
+
+#Creating a vector for three states with state names to draw coordinates
+statename<- c("vermont", "florida", "tennessee")
+
+#Adding a layer with points
+USmap2 <- USmap + geom_point(data=point, aes(x=lon, y=lat, color=LTR)) + scale_colour_gradient(low = 'black', high='red')
+
+#Scaling points by radius
+USmap2<- USmap2 + scale_radius(aes(size=point$LTR))
+
 
 # Creating png for the map.
 png(filename="map_usa_LTR_FL_TN_VT.png")
-map_usa_LTR
+USmap2
 dev.off()
+
 
 ## end your R code and logic 
 
