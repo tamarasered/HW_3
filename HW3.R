7b7648e04d4ebda988059f69fb4178ce93328bd3
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-17 00:44:11 -0400

    saving HW3.R

--- HW3.R
+++ HW3.R
@@ -11,6 +11,42 @@ df <- read.csv('clean_data.csv')
 ## start writing your R code from here
 library(ggplot2)
 
+
+
+# PART A.1: Generate a boxplot of LTR, which is already cleaned.
+
+# Creating a box plot
+myPlot <- ggplot(df, aes(x=Likelihood_Recommend_H, y=Likelihood_Recommend_H)) + geom_boxplot(fill="yellow", col="black")
+myPlot <- myPlot + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
+
+# Invoke the plot to draw it. 
+myPlot
+
+#Creating the png file of the boxplot.
+png(filename="box_LTR.png")
+myPlot
+dev.off()
+
+
+# PART A.2: Generate a boxplot for each NPS Type. 
+# The data.
+myPlot_NPS <- ggplot(df, aes(x=NPS_Type, y=Likelihood_Recommend_H))
+# The geometry.
+myPlot_NPS <- myPlot_NPS + geom_boxplot(fill="orange", col="black") 
+myPlot_NPS <- myPlot_NPS + scale_y_continuous( breaks = 1:10) + xlab("NPS type") + ylab("Likelihood to recommend")
+
+# Invoke the plot to draw it. 
+myPlot_NPS
+
+#Creating the png file of the boxplot.
+png(filename="box_NPS.png")
+myPlot_NPS
+dev.off()
+
+
+
+##################################################
+
 #Part B: 1
 # Creating a vector LTR
 LTR <- df$Likelihood_Recommend_H
@@ -52,6 +88,7 @@ dev.off()
 
 
 
+##################################################
 
 # Part C.1: Generate a scatterplot (x for hotel condition, y for LTR).
 
